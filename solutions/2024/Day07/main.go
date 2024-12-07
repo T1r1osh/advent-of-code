9f5d83e6e80da16e2c7876c3c76ebab417477f3a
Tamaska Roland <tamaska_roland@hotmail.com>
2024-12-07 12:12:45 +0100

    remove code duplications

--- solutions/2024/Day07/main.go
+++ solutions/2024/Day07/main.go
@@ -30,11 +30,11 @@ func main() {
 
 		newArr := make([]int, len(numbers)-1)
 		copy(newArr, numbers[1:])
-		task1 := Task1(goal, numbers[0], newArr)
+		task1 := Task1(goal, numbers[0], newArr, false)
 		if task1 {
 			task1sum = task1sum + goal
 		}
-		task2 := Task2(goal, numbers[0], newArr)
+		task2 := Task1(goal, numbers[0], newArr, true)
 		if task2 {
 			task2sum = task2sum + goal
 		}
@@ -43,24 +43,14 @@ func main() {
 	log.Println(task2sum)
 }
 
-func Task1(target, current int, arr []int) bool {
+func Task1(target, current int, arr []int, allowConcat bool) bool {
 	if len(arr) == 0 {
 		//finished
 		return target == current
 	}
 	newArr := make([]int, len(arr)-1)
 	copy(newArr, arr[1:])
-	return Task1(target, current*arr[0], newArr) || Task1(target, current+arr[0], newArr)
-}
-
-func Task2(target, current int, arr []int) bool {
-	if len(arr) == 0 {
-		//finished
-		return target == current
-	}
-	newArr := make([]int, len(arr)-1)
-	copy(newArr, arr[1:])
-	return Task2(target, current*arr[0], newArr) || Task2(target, current+arr[0], newArr) || Task2(target, concatNums(current, arr[0]), newArr)
+	return Task1(target, current*arr[0], newArr, allowConcat) || Task1(target, current+arr[0], newArr, allowConcat) || (allowConcat && Task1(target, concatNums(current, arr[0]), newArr, allowConcat))
 }
 
 func concatNums(num1, num2 int) int {
